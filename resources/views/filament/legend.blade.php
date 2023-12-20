0f1276fed75deeb689817522c19867d9d71f0582
ddevin96 <ddevinco@unisa.it>
2023-12-20 14:46:57 +0000

    rounded corner

--- resources/views/filament/legend.blade.php
+++ resources/views/filament/legend.blade.php
@@ -7,54 +7,52 @@
     }
     .leg td,
     .leg th {
-        /* border: 1px solid #ddd; */
         padding: 8px;
-        background-color: #18181b; <----------------------
+        background-color: #18181b;
         border-radius: 10px; */
-
     }
     .leg th {
-        padding-top: 12px;
-        padding-bottom: 12px;
         text-align: left;
-        background-color: #4CAF50;
-        color: white;
+    }
+    .tablecontainer {
+        background-color: #18181b;
+        border-radius: 15px;
+        padding: 20px;
     }
 </style>
-<table class="leg">
-    <thead>
-        <tr>
-            <!-- <th>name</th>
-            <th>type</th>
-            <th>a</th>
-            <th>b</th> -->
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Name</td>
-            <td>Name of hypergraph</td>
-        </tr>
-        <tr>
-            <td>|V|</td>
-            <td>Number of nodes</td>
-        </tr>
-        <tr>
-            <td>|E|</td>
-            <td>Number of hyperedges</td>
-        </tr>
-        <tr>
-            <td>Author</td>
-            <td>User that uploaded the hypergraph</td>
-        </tr>
-        <tr>
-            <td>Created at</td>
-            <td>Time of upload</td>
-        </tr>
-        <tr>
-            <td>Updated at</td>
-            <td>Last update</td>
-        </tr>
-    </tbody>
-</table>
-
+<div class="tablecontainer">
+    <table class="leg">
+        <thead>
+            <tr>
+                <th>Legend</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Name</td>
+                <td>Name of hypergraph</td>
+            </tr>
+            <tr>
+                <td>|V|</td>
+                <td>Number of nodes</td>
+            </tr>
+            <tr>
+                <td>|E|</td>
+                <td>Number of hyperedges</td>
+            </tr>
+            <tr>
+                <td>Author</td>
+                <td>User that uploaded the hypergraph</td>
+            </tr>
+            <tr>
+                <td>Created at</td>
+                <td>Time of upload</td>
+            </tr>
+            <tr>
+                <td>Updated at</td>
+                <td>Last update</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
