13934dd2158be6c97ebed0eb200f8fa1fda49bf2
ddevin96 <ddevinco@unisa.it>
2025-01-20 10:28:31 +0000

    update path and conf

--- app/Filament/Resources/JupyterNotebookResource.php
+++ app/Filament/Resources/JupyterNotebookResource.php
@@ -47,7 +47,7 @@ class JupyterNotebookResource extends Resource
                     ->color('primary')
                     ->url('#')
                     ->extraAttributes(fn($record) => [
-                        'data-url' => asset('http://localhost:8080/notebooks/' . $record->name)
+                        'data-url' => asset('https://hypergraphrepository.di.unisa.it:8888/notebooks/' . $record->name)
                     ]),
             ])
             ->headerActions([
